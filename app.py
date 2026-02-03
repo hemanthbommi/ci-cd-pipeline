21db5d663e7dd93c9c46202d7c3702ee740c9a54
hemanthbommi <bommihemanthkumar2003@gmail.com>
2026-02-03 22:25:37 +0530

    updated flask app version

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 
-from flask import Flask
+from flask import Flask, jsonify
+import socket
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Working Successfully!"
+    return jsonify({
+        "message": "CI/CD Pipeline Working Successfully",
+        "version": "v2.0",
+        "server": socket.gethostname(),
+        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+
+@app.route("/health")
+def health():
+    return {"status": "UP"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
